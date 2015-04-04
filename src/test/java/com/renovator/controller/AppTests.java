785e3703b641415e0156210043fdb2f4270fe1d7
darlingtld <darlingtld@qq.com>
2015-04-04 13:16:06 +0800

    add some user service code

--- src/test/java/com/renovator/controller/AppTests.java
+++ src/test/java/com/renovator/controller/AppTests.java
@@ -1,18 +1,23 @@
 package com.renovator.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.renovator.pojo.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,9 +36,17 @@ public class AppTests {
     }
 
     @Test
-    public void simple() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("hello"));
+    public void addUser() throws Exception {
+        User user=new User();
+        user.setName("¡È¥Ô");
+        user.setAddress("lingda º“");
+        user.setBalance(99.8);
+        user.setBirthday(new Date());
+        user.setContact("1340218638");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userJson = objectMapper.writeValueAsString(user);
+        System.out.println(userJson);
+        mockMvc.perform(post("/user/add").content(userJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(print());
     }
 }

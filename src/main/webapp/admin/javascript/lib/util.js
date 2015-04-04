8f1464227c1fafbd04173dcf20c6cb1210ea90e0
SmartSara <stoneapple0920@163.com>
2015-04-04 16:17:27 +0800

    zhuzhu is lovely~

---
+++ src/main/webapp/admin/javascript/lib/util.js
@@ -0,0 +1,26 @@
+/**
+ * Created by zhus1 on 2015/4/4.
+ */
+function formatDate(date, format) {
+    var o = {
+        "M+": date.getMonth() + 1, //month
+        "d+": date.getDate(),    //day
+        "h+": date.getHours(),   //hour
+        "m+": date.getMinutes(), //minute
+        "s+": date.getSeconds(), //second
+        "q+": Math.floor((date.getMonth() + 3) / 3),  //quarter
+        "S": date.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o) if (new RegExp("(" + k + ")").test(format))
+        format = format.replace(RegExp.$1,
+            RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+}
+
+function getURLParameter(key) {
+    var reg = new RegExp("(^|&)" + key + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}

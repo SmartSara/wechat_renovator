9b3f5f79916b40eab05808eafae02675fb35173e
SmartSara <stoneapple0920@163.com>
2015-04-06 16:08:55 +0800

    smart zhuzhu~

--- src/main/webapp/admin/javascript/product.js
+++ src/main/webapp/admin/javascript/product.js
@@ -1,13 +1,116 @@
 /**
  * Created by zhus1 on 2015/4/4.
  */
+var _product;
+var _saveOrUpdate;
 
 $(function () {
     _initialNav();
+    _initialPage();
 });
 
-function _initialNav(){
-    $('#nav').load("nav.html", function() {
+function _initialNav() {
+    $('#nav').load("nav.html", function () {
         $("#productNav").addClass("active");
     });
+}
+
+function _initialPage() {
+    $.ajax({
+        type: "get",
+        url: "/product/list",
+        dataType: "json",
+        success: function (data) {
+            $("#productTemplate").tmpl(data).appendTo("#productList");
+            //pageTable("#pagination", "#packageList tr",num_per_page);
+            _product = data;
+            $("#loading").addClass("hidden");
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(textStatus);
+            console.log(XMLHttpRequest.status);
+            console.log(XMLHttpRequest.responseText);
+        }
+    });
+}
+
+$(document).on("click", ".update-product", function () {
+    _saveOrUpdate = "update";
+    var _thisProduct = _getProductByProductId($(this).data('id'));
+    $(".modal-body #productId").val($(this).data('id'));
+    $(".modal-body #productName").val(_thisProduct.name);
+    $(".modal-body #productDescription").val(_thisProduct.description);
+    $(".modal-body #productPrice").val(_thisProduct.price);
+    $(".modal-body #productDiscount").val(_thisProduct.discount);
+});
+
+
+function _saveOrUpdateProduct() {
+    var product = {};
+    if (_saveOrUpdate == "update") {
+        product.id = $("#productId").val();
+    } else {
+        _saveOrUpdate = "add";
+    }
+    product.name = $("#productName").val();
+    product.description = $("#productDescription").val();
+    product.price = $("#productPrice").val();
+    product.discount = $("#productDiscount").val();
+    product.ts = new Date();
+
+    $.ajax({
+        type: "post",
+        url: "/product/" + _saveOrUpdate,
+        contentType: "application/json",
+        data: JSON.stringify(product),
+        success: function (data) {
+            $("#loading").addClass("hidden");
+            location.reload();
+        },
+        error: function (data) {
+            $("#loading").addClass("hidden");
+            alert(data.status);
+        }
+    });
+}
+
+function _deleteProductById(id) {
+    BootstrapDialog.show({
+        title: '删除产品信息',
+        message: '<div class="alert alert-danger" role="alert"><span class="label label-warning">Warning</span> 确认删除该产品？</div>',
+        buttons: [{
+            label: '删除',
+            cssClass: 'btn-warning',
+            action: function (dialogItself) {
+                $("#loading").removeClass("hidden");
+                $.ajax({
+                    type: "post",
+                    url: "/product/delete?id=" + id,
+                    success: function (data) {
+                        $("#loading").addClass("hidden");
+                        dialogItself.close();
+                        location.reload();
+                    },
+                    error: function (data) {
+                        $("#loading").addClass("hidden");
+                        alert(data.status);
+                    }
+                });
+            }
+        }, {
+            label: '取消',
+            cssClass: 'btn-primary',
+            action: function (dialogItself) {
+                dialogItself.close();
+            }
+        }]
+    });
+}
+
+function _getProductByProductId(productId) {
+    for (var i = 0; i < _product.length; i++) {
+        if (_product[i].id == productId) {
+            return _product[i];
+        }
+    }
 }

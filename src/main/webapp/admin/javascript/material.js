fee7db9a46f47192b60b2c0efaad589188cda879
粥五 <dewei.zdw@alibaba-inc.com>
2015-05-10 17:44:52 +0800

    fix delte function bugs

--- src/main/webapp/admin/javascript/material.js
+++ src/main/webapp/admin/javascript/material.js
@@ -182,9 +182,9 @@ function initMaterialsEvent(){
 	//Remove
 		$("#materials").on("click",".del",function(){
 			
+			var articleId = $(this).parents(".singleArticle").data("id");
 			var delDialogEle='<div class="modal fade delDialog" tabindex="-1" role="dialog" aria-labelledby="title" aria-hidden="false"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button> <h4 class="modal-title" id="title">提醒</h4> </div> <div class="modal-body text-center">您确定要删除？</div> <div class="modal-footer"> <button type="button" class="no btn btn-default" data-dismiss="modal">取消</button><button type="button" class="ok btn btn-default" data-dismiss="modal">确定</button> </div> </div> </div>';
 			$(delDialogEle).modal("show").on("click",".ok",function(){
-				var articleId = $(this).parents(".singleArticle").data("id");
 				var url =  "../../material/article/del/"+articleId;
 				$.ajax(url).done(
 						function(data){

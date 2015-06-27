63856021a19c863ef7ef8d05a013755219348ecf
darlingtld <darlingtld@qq.com>
2015-06-27 10:56:12 +0800

    bug fix

--- src/main/webapp/mall/js/mall.js
+++ src/main/webapp/mall/js/mall.js
@@ -15,9 +15,9 @@ mallModule.controller('homeController', function ($scope, $http, $location, $roo
         $scope.itemLatest = $scope.items.slice(start, start + 3);
         $scope.itemMainShow = $scope.items.slice(start + 3, start + 9);
         $scope.itemOthers = $scope.items.slice($scope.items.length - 4, $scope.items.length);
-        $('.banner').css('background', 'url(/repository/images/banner.png) no-repeat center center');
+        $('.banner').css('background', 'url(../repository/images/banner.png) no-repeat center center');
         $('.banner').css('background-size', 'cover');
-        $('.in-left').css('background', 'url(/repository/images/luxury_img_a.jpg) 0px 0px');
+        $('.in-left').css('background', 'url(../repository/images/luxury_img_a.jpg) 0px 0px');
 
     });
 
